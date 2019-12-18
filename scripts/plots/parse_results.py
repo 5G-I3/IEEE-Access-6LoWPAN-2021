2c439de20f7d3b193371c99e42232b87500cbeaf
Martine S. Lenders <m.lenders@fu-berlin.de>
2019-12-18 15:37:06 +0100

    scripts: update experiment names

--- scripts/plots/parse_results.py
+++ scripts/plots/parse_results.py
@@ -23,7 +23,7 @@ DATA_PATH = os.environ.get("DATA_PATH",
                            os.path.join(SCRIPT_PATH, "..", "..", "results"))
 GLOBAL_PREFIX = os.environ.get("GLOBAL_PREFIX", "2001:db8:0:1:")
 
-NAME_PATTERN = r"lcn19_" \
+NAME_PATTERN = r"6lo_comp_" \
                r"n(?P<network>m3-\d+x[0-9a-f]+)_c\d+__" \
                r"m{mode}_r{data_len}Bx\d+x{delay}ms_(?P<timestamp>\d+)"
 LOG_NAME_PATTERN = r"{}\.log".format(NAME_PATTERN.format(
